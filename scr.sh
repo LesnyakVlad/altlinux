8e88ce03278922f8cd3dfbde95ddfa5511899ef9
LesnyakVlad <61940748+LesnyakVlad@users.noreply.github.com>
2021-08-28 14:31:20 +0300

    Create scr.sh

---
+++ scr.sh
@@ -0,0 +1,58 @@
+   46  sudo rpm -i https://ftp.postgresql.org/pub/pgadmin/pgadmin4/yum/pgadmin4-fedora-repo-2-1.noarch.rpm
+   47  sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
+   48  sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
+   49  sudo apt install pgadmin4
+   50  apt install pgadmin4
+   51  apt-get install pgadmin3
+   52  /etc/apt/sources.list.d/pgadmin4.list.
+   53  /etc/apt/sources.list.d/pgadmin4.list.
+   54  vim /etc/apt/sources.list.d/pgadmin4.list
+   55  apt-get install pgadmin3
+   56  curl https://www.pgadmin.org/static/packages_pgadmin_org.pub |  apt-key add
+   57  curl https://www.pgadmin.org/static/packages_pgadmin_org.pub
+   58  postgresql12-server
+   59  postgresql11-server
+   60  postgresql10-server
+   61  postgresql10-1C-server
+   62  postgresql9.4-server
+   63  postgresql9.5-server
+   64  postgresql9.6-server
+   65  apt-get update
+   66  /etc/init.d/portgresql initdb
+   67  mkdir /etc/init.d/portgresql initdb
+   68  apt-get install postgresql12-server
+   69  chkconfig postgersql on
+   70  systemctl enable postgresql 
+   71  python3 -m venv venv
+   72  source venv/bin/activate
+   73  pip install --upgrade pip
+   74  PATH=$PATH:/usr/local/pgsql/bin pip install -r $PGADMIN4_SRC/requirements.txt
+   75  pip install -r $PGADMIN4_SRC/web/regression/requirements.txt
+   76  python3 $PGADMIN4_SRC/web/setup.py
+   77  cd /
+   78  python3 $PGADMIN4_SRC/web/setup.py
+   79  sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list
+   80  123
+   81  vim /etc/apt/sources.list.d/pgadmin4.list
+   82  su 
+   83  ho
+   84  mc
+   85  mc
+   86  mc
+   87  /usr/lib/mc/extfs.d/rpm run /home/user/Загрузки/pgadmin4-redhat-repo-2-1.noarch.rpm INSTALL
+   88  /usr/lib/mc/extfs.d/rpm run /home/user/Загрузки/pgadmin4-redhat-repo-2-1.noarch.rpm UPGRADE
+   89  su
+   90  https://ftp.postgresql.org/pub/pgadmin/pgadmin4/yum/pgadmin4-redhat-repo-2-1.noarch.rpm
+   91  cp https://ftp.postgresql.org/pub/pgadmin/pgadmin4/yum/pgadmin4-redhat-repo-2-1.noarch.rpm /home/user/Загрузки/
+   92  ьс
+   93  mc
+   94  chmod +rwx pgadmin4-redhat-repo-2-1.noarch.rpm 
+   95  mc
+   96  chmod -r pgadmin4-redhat-repo-2-1.noarch.rpm 
+   97  mc
+   98  apt-get install pgadmin3
+   99  apt-get install pgadmin3-docs-all 
+  100  cd /
+  101  service postgresql start
+  102  systemctl enable postgresql
+  103  history
