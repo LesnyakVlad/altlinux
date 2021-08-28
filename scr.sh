cc80fdd8a51a7d0d1be765782fc0e4dbbaf2ec03
LesnyakVlad <61940748+LesnyakVlad@users.noreply.github.com>
2021-08-28 16:53:07 +0300

    Update scr.sh

--- scr.sh
+++ scr.sh
@@ -1,7 +1,7 @@
 #!bin/bash
    apt-get update
    apt-get install postgresql12 
-   apt-get install postgresql12-contrib 
+   apt-get install postgresql12-contrib
    apt-get install postgresql12-perl 
    apt-get install postgresql12-python 
    apt-get install postgresql12-server 
@@ -9,12 +9,13 @@
    apt-get install postgresql12-1C-contrib  
    apt-get install postgresql12-1C-perl 
    apt-get install postgresql12-1C-python 
-   apt-get install postgresql12-1C-server 
-   apt-get install postgresql12-1C-tcl 
+   apt-get install postgresql12-1C-server
+   apt-get install postgresql12-1C-tcl
    apt-get update
+   mkdir -p /var/lib/pgadmin3/ /var/log/pgadmin3/
+   chmod +rwx pgadmin3-1.23.0-0.2.git705eb1b.fc33.aarch64.rpm
    ./pgadmin3-1.23.0-0.2.git705eb1b.fc33.aarch64.rpm
    apt-get install pgadmin3
-   apt-get install pgadmin3-docs-all 
+   apt-get install pgadmin3-docs-all
+   systemctl restart httpd
    systemctl enable postgresql
-   apt-get update
-   chmod +rwx /lib/systemd/systemd-sysv-install
