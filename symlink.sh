5d6778b88e8c76b48157324ff81c5d6482ff5c9c
LesnyakVlad <61940748+LesnyakVlad@users.noreply.github.com>
2021-11-18 12:26:30 +0300

    Update symlink.sh

--- symlink.sh
+++ symlink.sh
@@ -1,7 +1,12 @@
 #!bin/bash
+sh kernel.sh
+cd /etc/modprobe.d/
+rm -f blacklist-video.conf
+touch blacklist-video.conf
+echo "blacklist nouveau" >> blacklist-video.conf
 
 cd /lib/modules/4.19.102-std-def-alt1/nVidia
-rm -r nvidia*
+rm -f nvidia*
 ln -s /lib/modules/nvidia/drm-4.19.102-std-def-alt1-390.132 /lib/modules/4.19.102-std-def-alt1/nVidia/nvidia-drm.ko
 ln -s /lib/modules/nvidia/4.19.102-std-def-alt1-390.132 /lib/modules/4.19.102-std-def-alt1/nVidia/nvidia.ko
 ln -s /lib/modules/nvidia/modeset-4.19.102-std-def-alt1-390.132 /lib/modules/4.19.102-std-def-alt1/nVidia/nvidia-modeset.ko
